72121c1458d831e7b70cd7ce541d9d05002810f6
taranvd <apple.vlad.taran@gmail.com>
2024-05-01 12:03:40 +0300

    refactor: Updated custom error classes: removed comments

--- packages/backend/src/utils/custom-errors.ts
+++ packages/backend/src/utils/custom-errors.ts
@@ -38,7 +38,6 @@ export class ConflictError extends CustomError {
 		super(message, 409);
 	}
 }
-// Помилки сервера (5xx)
 export class InternalServerError extends CustomError {
 	constructor(message: string = 'Internal Server Error') {
 		super(message, 500);
