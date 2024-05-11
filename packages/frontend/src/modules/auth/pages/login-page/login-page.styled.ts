9f67251010a9232be13a2ba66b1af060f3d96df9
taranvd <apple.vlad.taran@gmail.com>
2024-05-11 16:41:09 +0300

    refactor: remove comments

--- packages/frontend/src/modules/auth/pages/login-page/login-page.styled.ts
+++
@@ -1,7 +0,0 @@
-// import { css } from '@emotion/css';
-
-// export const styles = {
-// 	container: css`
-// 		width: 100%;
-// 	`,
-// };
--- packages/frontend/src/shared/components/form-groups-todo/form-groups.component.tsx
+++ packages/frontend/src/shared/components/form-groups-todo/form-groups.component.tsx
@@ -1,13 +1,11 @@
-// form-groups.component.tsx
 import React from 'react';
 import { Field } from 'react-final-form';
 import { FormGroup, InputGroup } from '@blueprintjs/core';
 import styles from './form-groups.styled';
-import InputError from '../input-error/input-error.component';
 
 const FormGroups: React.FC = () => {
 	return (
-		<div>
+		<>
 			<FormGroup label="Title" className={styles.formGroup}>
 				<Field name="title">
 					{({ input, meta }) => (
@@ -39,7 +37,7 @@ const FormGroups: React.FC = () => {
 					)}
 				</Field>
 			</FormGroup>
-		</div>
+		</>
 	);
 };
 
